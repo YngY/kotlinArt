c348e05b1b57145af05af94d192149441ac73983
yangyi <576256878@qq.com>
2021-02-07 10:21:16 +0800

    '创建了一个分支'

--- app/src/main/java/com/yy/kotlinart/BActivity.kt
+++ app/src/main/java/com/yy/kotlinart/BActivity.kt
@@ -2,11 +2,14 @@ package com.yy.kotlinart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlin.math.log
 
 class BActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_b)
+        Log.d("BActivity","first1")
     }
 
     override fun onRestart() {
